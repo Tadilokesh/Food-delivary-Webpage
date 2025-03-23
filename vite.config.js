bbb1ea3272bc64cda67c78bd841a205049bdc41f
Tadi <tadilokesh8@gmail.com>
2025-03-23 21:27:55 +0530

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/Food-delivary-Webpage/", // Add your repository name here
-})
